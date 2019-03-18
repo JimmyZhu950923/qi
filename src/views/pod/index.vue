f3840277c700ffa4ff8fcf176e714bf1a518e872
Wen Tian <tian.wen@venusource.com>
2019-03-18 13:41:22 +0800

    wt change table

--- src/views/pod/index.vue
+++ src/views/pod/index.vue
@@ -3,22 +3,28 @@
     <el-tabs>
       <el-tab-pane label="容器实例">
         <el-table
+          v-loading="loading"
           :data="podData"
+          element-loading-text="拼命加载中"
+          element-loading-spinner="el-icon-loading"
+          element-loading-background="rgba(0, 0, 0, 0.8)"
           style="width:100%"
           stripe
         >
           <el-table-column prop="metadata.namespace" label="命名空间" width="130"/>
           <el-table-column prop="metadata.name" label="实例名称" width="300"/>
+          <el-table-column prop="status.hostIP" label="READY" width="140">
+            <template slot-scope="scope">{{ getReady(scope.row.status.containerStatuses) }}/{{ scope.row.spec.containers.length }}</template>
+          </el-table-column>
           <el-table-column prop="status.phase" label="状态" width="120">
             <template slot-scope="scope">
               <font :color="checkStatusFunc(scope.row.status.phase)">{{ scope.row.status.phase }}</font>
             </template>
           </el-table-column>
-          <el-table-column prop="metadata.creationTimestamp" label="开始时间" width="150">
+          <el-table-column prop="metadata.creationTimestamp" label="AGE" width="150">
             <template slot-scope="scope">{{ time(scope.row.metadata.creationTimestamp) }}</template>
           </el-table-column>
-          <el-table-column prop="status.hostIP" label="所在主机IP" width="140"/>
-          <el-table-column prop="status.podIP" label="容器IP" width="140"/>
+          <el-table-column prop="status.containerStatuses[0].restartCount" label="命名空间" width="130"/>
           <el-table-column label="操作">
             <template slot-scope="scope">
               <el-button
@@ -38,7 +44,8 @@ import { getPods, update } from '@/api/pod'
 export default {
   data() {
     return {
-      podData: []
+      podData: [],
+      loading: true
     }
   },
   created() {
@@ -53,44 +60,27 @@ export default {
       getPods(data).then(response => {
         console.log(response.data.items)
         _this.podData = response.data.items
+        _this.loading = false
       })
     },
     resetFunc: function(data) {
       console.log(data)
     },
     time: function(tm) {
-      var data = new Date(tm)
-      var str =
-        data.getFullYear() +
-        '-'
-
-      if (data.getMonth().toString().length === 1) {
-        str += '0' + data.getMonth() + '-'
+      var createTime = new Date(tm).getTime()
+      var now = new Date().getTime()
+      var age = Math.floor((now - createTime) / 1000)
+      if (age < 60) {
+        return Math.floor(age) + 's'
+      } else if (age / 60 < 60) {
+        return Math.floor(age / 60) + 'm'
+      } else if (age / 60 / 60 < 24) {
+        return Math.floor(age / 60 / 60) + 'h'
       } else {
-        str += data.getMonth() + '-'
+        return Math.floor(age / 60 / 60 / 24) + 'd'
       }
-
-      if (data.getDate().toString().length === 1) {
-        str += '0' + data.getDate() + ' '
-      } else {
-        str += data.getDate() + ' '
-      }
-
-      if (data.getHours().toString().length === 1) {
-        str += '0' + data.getHours() + ':'
-      } else {
-        str += data.getHours() + ':'
-      }
-
-      if (data.getSeconds().toString().length === 1) {
-        str += '0' + data.getSeconds()
-      } else {
-        str += data.getSeconds()
-      }
-      return str
     },
     checkStatusFunc: function(phase) {
-      debugger
       if (phase === 'Running') {
         return '#46AF60'
       } else {
@@ -106,6 +96,19 @@ export default {
           message: '重建成功!'
         })
       })
+    },
+    getReady: function(containerStatuses) {
+      if (containerStatuses !== undefined) {
+        var flag = 0
+        containerStatuses.forEach(function(item) {
+          console.log(item)
+          if (item.ready) {
+            flag++
+          }
+        })
+        return flag
+      }
+      return 0
     }
   }
 }
