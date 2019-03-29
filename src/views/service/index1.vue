00abbc4238b627b97b1dcf4f746667d270df3896
edz <edz@Jimmy.local>
2019-03-29 14:38:41 +0800

    增加了详情

---
+++ src/views/service/index1.vue
@@ -0,0 +1,261 @@
+<template>
+  <div>
+    <el-main>
+      <el-dialog :visible.sync="dialogVisible" title="新建服务" width="35%" height="80%">
+        <el-form :model="selForm" label-width="80px">
+          <el-form-item label="服务名称">
+            <el-input v-model="selForm.name" class="searchClass" />
+          </el-form-item>
+          <el-form-item label="命名空间:">
+            <el-select v-model="selForm.namespace1">
+              <el-option
+                v-for="item in options4"
+                :key="item.metadata.name"
+                :label="item.metadata.name"
+                :value="item.metadata.name"/>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="服务类型">
+            <el-select v-model="selForm.type">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"/>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Port">
+            <el-input v-model="selForm.port" class="searchClass"/>
+          </el-form-item>
+        </el-form>
+        <span slot="footer">
+          <el-button @click="dialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="newService()">发布</el-button>
+        </span>
+      </el-dialog>
+      <el-row>
+        <el-col :span="15">
+          <el-button type="primary" size="mini" icon="el-icon-plus" round @click="dialogVisible = true">&nbsp;&nbsp;新建&nbsp;&nbsp;</el-button>
+        </el-col>
+        <el-col :span="7">
+          <el-input v-model="name" size="mini" clearable placeholder="请输入名称" class="input-with-select">
+            <el-select slot="prepend" v-model="namespace2" size="mini" placeholder="请选择" @change="getAllServices()">
+              <el-option
+                v-for="item in options4"
+                :key="item.metadata.name"
+                :label="item.metadata.name"
+                :value="item.metadata.name"
+              />
+            </el-select>
+            <el-button slot="append" size="mini" icon="el-icon-search" @click="nameChange()"/>
+          </el-input>
+        </el-col>
+        <el-col :span="1" style="margin-left:10px">
+          <el-button size="mini" icon="el-icon-refresh" circle @click="rr()"/>
+        </el-col>
+      </el-row>
+      <el-table
+        :data="tableData"
+        stripe
+        width="100%"
+        highlight-current-row
+      >
+        <el-table-column prop="metadata.name" label="名称" sortable width="280" >
+          <template slot-scope="scope">
+            <a @click="goIndex2(scope.row.metadata.name, scope.row.metadata.namespace)">{{ scope.row.metadata.name }}</a>
+          </template>
+        </el-table-column>
+        <el-table-column prop="spec.type" label="服务类型" sortable>
+          <template slot-scope="s">
+            {{ rag(s.row.spec.type) }}
+          </template>
+        </el-table-column>
+        <el-table-column prop="spec.clusterIP" label="端点" sortable/>
+        <el-table-column prop="metadata.namespace" label="命名空间" sortable/>
+        <el-table-column label="操作" width="165">
+          <template slot-scope="scope">
+            <el-button
+              size="small"
+              type="danger"
+              @click="delService(scope.row.metadata)">
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        :current-page="currentPage"
+        :total="countPage"
+        :page-size="pageSize"
+        layout="total,prev,pager,next"
+        @current-change="handlePageChange"
+      />
+    </el-main>
+  </div>
+</template>
+
+<script>
+import { getServices, getSingle, addServices, remove } from '@/api/service'
+import { getAllNamespace } from '@/api/namespace'
+export default {
+  data() {
+    return {
+      name: '',
+      namespace: '',
+      dialogVisible: false,
+      tableData: [],
+      sels: [],
+      input: '',
+      selection: true,
+      selForm: {
+        metadata: {
+          name: '',
+          namespace: ''
+        },
+        spec: {
+          ports: [{
+            port: 0
+          }],
+          type: ''
+        }
+      },
+      options: [{
+        value: 'ClusterIP',
+        label: '集群IP'
+      }, {
+        value: 'NodePort',
+        label: '节点端口'
+      }],
+      options4: [],
+      stripe: true,
+      namespace1: '',
+      namespace2: '',
+      value: '',
+      pageSize: 20,
+      currentPage: 1,
+      countPage: 0
+    }
+  },
+  created() {
+    this.getAllServices()
+    this.selNameSpace()
+  },
+  methods: {
+    getAllServices: function() {
+      // debugger
+      const _this = this
+      var data = {
+        namespace: _this.namespace1
+      }
+      getServices(data).then(response => {
+        console.log(response)
+        _this.tableData = response.data.items
+        _this.countPage = response.data.items.length
+      })
+    },
+    getSingleService: function() {
+      // debugger
+      const _this = this
+      if (_this.namespace1 === '') {
+        _this.$message({
+          type: 'danger',
+          message: '请先选择命名空间'
+        })
+      } else {
+        var namespace = _this.namespace1
+        var name = _this.name
+        _this.tableData = []
+        getSingle(namespace, name).then(response => {
+          console.log(response)
+          _this.tableData.push(response.data)
+          _this.countPage = 1
+        })
+      }
+    },
+    nameChange: function() {
+      this.getSingleService()
+    },
+    newService: function() {
+      // debugger
+      const _this = this
+      var name = this.selForm.name
+      var namespace = this.selForm.namespace1
+      var type = this.selForm.type
+      var port = this.selForm.port
+      addServices(name, namespace, type, port).then(response => {
+        _this.$message({
+          type: 'success',
+          message: '新建成功!'
+        })
+        _this.dialogVisible = false
+        _this.selForm.name = ''
+        _this.selForm.namespace = ''
+        _this.selForm.type = ''
+        _this.selForm.port = 0
+        _this.getAllServices()
+      })
+    },
+    delService: function(metadata) {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          // debugger
+          var data = { name: metadata.name, namespace: metadata.namespace }
+          remove(data).then(response => {
+            this.getAllServices()
+            this.$message({
+              type: 'success',
+              message: '删除成功'
+            })
+          })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+    },
+    selNameSpace: function() {
+      getAllNamespace().then(response => {
+        // debugger
+        this.options4 = response.data.items
+      })
+    },
+    rr: function() {
+      this.namespace = null
+      this.namespace1 = null
+      this.name = null
+      this.port = null
+      this.getAllServices()
+    },
+    rag: function(data) {
+      var result = data
+      if (data === 'ClusterIP') {
+        result = '集群IP'
+      } else if (data === 'NodePort') {
+        result = '节点端口'
+      }
+      return result
+    },
+    handlePageChange: function(page) {
+      this.currentPage = page
+      this.getAllServices()
+    },
+    goIndex2(name, namespace) {
+      this.$router.push({ name: 'Service2', params: { name: name, namespace: namespace }})
+    }
+  }
+}
+</script>
+<style>
+  .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+</style>
---
+++ src/views/service/index2.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="Show">
+    <el-container>
+      <el-main v-loading="loading">
+        <el-tabs>
+          <el-tab-pane label="详情">
+            <el-row>
+              <el-col :span="4">名称:</el-col>
+              <el-col :span="20">{{ serviceName }}</el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="4">命名空间:</el-col>
+              <el-col :span="20">{{ serviceNamespace }}</el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="4">标签:</el-col>
+              <el-col :span="20">
+                <el-tag v-for="(label,value) in labels" :key="label">{{ value }}:{{ label }}</el-tag>
+              </el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="4">创建时间:</el-col>
+              <el-col :span="20">{{ time1(creationTimestamp) }}</el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="4">标签选择器:</el-col>
+              <el-col :span="20">
+                <el-tag v-for="(label,value) in selector" :key="label">{{ value }}:{{ label }}</el-tag>
+              </el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="4">类型:</el-col>
+              <el-col :span="20">{{ serviceType }}</el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="4">保持会话:</el-col>
+              <el-col :span="20">{{ keepConversation }}</el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="24"><h4>连接</h4></el-col>
+            </el-row>
+            <el-row>
+              <el-col :span="4">集群IP:</el-col>
+              <el-col :span="20">{{ clusterIP }}</el-col>
+            </el-row><br>
+            <el-row>
+              <el-col :span="4">内部端点:</el-col>
+              <el-col :span="20">{{ innerPort }}</el-col>
+            </el-row><br>
+          </el-tab-pane>
+        </el-tabs>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+<script>
+import { getServices, getSingle } from '@/api/service'
+export default {
+  data() {
+    return {
+      serviceName: '',
+      serviceNamespace: '',
+      annotaions: '',
+      creattionTimestamp: '',
+      serviceType: '',
+      keepConversation: '',
+      clusterIP: '',
+      innerPort: '',
+      labels: [],
+      selector: []
+    }
+  },
+  created() {
+    this.selS()
+    this.getAllServices()
+  },
+  methods: {
+    selS: function() {
+      this.serviceName = this.$route.params.name
+      this.serviceNamespace = this.$route.params.namespace
+      getSingle(this.serviceNamespace, this.serviceName).then(response => {
+        debugger
+        this.serviceName = response.data.metadata.name
+        this.serviceNamespace = response.data.metadata.namespace
+        this.serviceType = response.data.spec.type
+        this.clusterIP = response.data.spec.clusterIP
+        this.innerPort = response.data.metadata.name + ':' + response.data.spec.ports[0].port + ' ' + response.data.spec.ports[0].protocol
+        this.keepConversation = response.data.spec.sessionAffinity
+        this.creationTimestamp = response.data.metadata.creationTimestamp
+        this.labels = response.data.metadata.labels
+        this.selector = response.data.spec.selector
+      })
+    },
+    getAllServices: function() {
+      // debugger
+      const _this = this
+      var data = {
+        serviceName: _this.serviceName,
+        serviceNamespace: _this.serviceNamespace
+      }
+      getServices(data).then(response => {
+        _this.serviceData = response.data.item
+        _this.loading = false
+      })
+    },
+    time1: function(tm) {
+      var data = new Date(tm)
+      var str =
+        data.getFullYear() +
+        '/' +
+        (data.getMonth() + 1) +
+        '/' +
+        data.getDate() +
+        ' '
+      if (data.getHours() < 12) {
+        str += '上午' + data.getHours() + ':'
+      } else {
+        str += '下午' + (data.getHours() - 12) + ':'
+      }
+      if (data.getSeconds().toString().length === 1) {
+        str += '0' + data.getSeconds()
+      } else {
+        str += data.getSeconds()
+      }
+      return str
+    }
+  }
+}
+</script>
+<style>
+.Show .el-tag{
+  font-size:15px;
+  border-radius: 20px;
+  margin-right: 15px;
+  margin-bottom: 10px;
+}
+.Show{
+font-size:15px;
+}
+.el-header{
+  padding-bottom: 0%;
+}
+</style>
